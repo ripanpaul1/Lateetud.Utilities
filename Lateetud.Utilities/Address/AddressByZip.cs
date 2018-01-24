26a8d71d53cefa5a3597273f730b393030dbb0ed
Ripan Paul <ripan.paul@lateetud.com>
2018-01-24 16:50:59 +0530

    1st commit

---
+++ Lateetud.Utilities/Address/AddressByZip.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Lateetud.Utilities.Address
+{
+    public class AddressByZipResult
+    {
+        public string country { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+    }
+
+    public class AddressByZip
+    {
+        // NOTE: all query string parameter values must be URL-encoded!
+        private readonly string url;
+
+        public AddressByZip(string apiUrl, string ZipCode)
+        {
+            this.url = apiUrl + ZipCode;
+        }
+
+        public string Execute()
+        {
+            using (var client = new WebClient())
+                return client.DownloadString(this.url);
+        }
+    }
+
+ 
+}
---
+++ Lateetud.Utilities/StaticUtilities.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+
+namespace Lateetud.Utilities
+{
+    public static class StaticUtilities
+    {
+        public static void SendErrorNotification(string Subject, string Body, Exception Ex,HttpRequest TheRequest)
+        {
+            try
+            {
+                Subject = ConfigurationManager.AppSettings["ErrorSubject"] + Subject;
+                Page ThePage = HttpContext.Current.Handler as Page;
+                ThePage.Server.ClearError();
+                if (TheRequest == null)
+                    TheRequest = ThePage.Request;
+                if (Ex != null)
+                {
+                    Ex=Ex.GetBaseException();
+                    Body += "\n\nMessage: " + Ex.Message + "\n\n"
+                    + "Source: " + Ex.Source + "\n\n"
+                    + Ex.StackTrace;
+                    Subject+= ": " + Ex.Message.Replace("\n","").Replace("\r","");
+                }
+                Body += "\n\n" + TheRequest.Url.ToString() + "\n\n" + TheRequest.UserHostAddress + "\n\n";
+
+                string Address = ConfigurationManager.AppSettings["ErrorAddress"].ToString();
+                MailMessage Mail = new MailMessage(Address,Address);
+                Mail.Subject = Subject;
+                Mail.IsBodyHtml = false;
+                Mail.Body = Body;
+                SmtpClient MailServer = new SmtpClient(ConfigurationManager.AppSettings["SMTPServer"]);
+                MailServer.Send(Mail);
+            }
+            catch
+            {
+            }
+        }
+        public static string FormatMoney(decimal Value)
+        {
+            return String.Format("{0:0.00}", Value);
+        }
+        public static bool IsAppSettingTrue(string Key)
+        {
+            return ConfigurationManager.AppSettings[Key] != null && Convert.ToBoolean(ConfigurationManager.AppSettings[Key]);
+        }
+        public static bool IsApSettingEqual(string Key, string Value)
+        {
+            return ConfigurationManager.AppSettings[Key] != null && ConfigurationManager.AppSettings[Key].ToString()==Value;
+        }
+        public static string Truncate(string TheString, int Length)
+        {
+            return TheString.Substring(0, Math.Min(TheString.Length, Length));
+        }
+        public static Control FindControlRecursive(Control Root, string Id)
+        {
+            if (Root.ID == Id)
+                return Root;
+            foreach (Control Ctl in Root.Controls)
+            {
+                Control FoundCtl = FindControlRecursive(Ctl, Id);
+                if (FoundCtl != null)
+                    return FoundCtl;
+            }
+            return null;
+        }
+    }
+}
