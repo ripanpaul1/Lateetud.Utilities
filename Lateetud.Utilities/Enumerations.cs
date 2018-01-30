108097c8b94115483d63c07bcd19542b9dd87f09
Ripan Paul <ripan.paul@lateetud.com>
2018-01-30 17:47:33 +0530

    added xml manage

---
+++ Lateetud.Utilities/Enumerations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lateetud.Utilities
+{
+    public enum PStatus
+    {
+        None = 0,
+        Success = 1,
+        Failed = 2,
+        Error = 3
+    }
+}
---
+++ Lateetud.Utilities/XmlManager/XmlService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace Lateetud.Utilities.XmlManager
+{
+    public class XmlService
+    {
+        public bool CreateXml(string XmlContent, string XmlPath, string FileName)
+        {
+            return this.CreateXml(XmlContent, Path.Combine(XmlPath, FileName));
+        }
+        public bool CreateXml(string XmlContent, string XmlPathWithFileName)
+        {
+            try
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.LoadXml(XmlContent);
+                xDoc.Save(XmlPathWithFileName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
